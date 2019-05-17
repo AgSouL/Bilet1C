b1bb36e77fd9fff3ce802b3cc26a275db3ebbe2f
Ag SouL <47560765+AgSouL@users.noreply.github.com>
2019-05-17 10:15:43 +0300

    Add files via upload

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Password_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                if (textBox1.Text == "admin1" && maskedTextBox1.Text == "admin1") 
+                {
+                    MessageBox.Show("Отлично!");
+                } else {
+                    MessageBox.Show("Введенные данные не верны!");
+                }
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
